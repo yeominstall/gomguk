62a4b673049d8ec9bda43269edd276ec8af8e93c
Yeomin Nam <y.minn13@gmail.com>
2017-05-30 17:42:01 +0900

    e_runner.sh for ramsmp

--- e_runner.sh
+++ e_runner.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-pnum=( 8 )
+pnum=( 1 2 4 8 )
 
-for pn in ${pnum}; do
+for pn in 1 2 4 8
+do
 
 	sleep 5
-	echo "cpubomb thread ${pn}"
+	echo "ramsmp -b1 -p ${pn}"
 
 	{ time ../odroid-smartpower-linux/smartpower energy/smartresult.txt & } 2> ./energy/time.txt
-	./cpubomb/runn/cpubomb ${pn}
+	./ramsmp-3.5.0-custom/ramsmp -b 1 -p ${pn}
 
 	killall smartpower
 
-	tail -n 1 ./energy/smartresult.txt >> energy/res.txt
+	tail -n 1 energy/smartresult.txt >> energy/res.txt
 	echo -e "\n\n"
 
 	tail -n 3 energy/time.txt >> energy/res.txt
 	echo -e "\n\n"
-	echo "cpubomb thread ${pn} end"
+	echo "ramsmp -b1 -p ${pn} end"
 done
