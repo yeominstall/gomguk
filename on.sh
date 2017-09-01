44cb81f4406d1a65ad50533c0c0fc901ae058ab7
Yeomin Nam <y.minn13@gmail.com>
2017-09-01 22:14:57 +0900

    iozone bL result

---
+++ on.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo 1 > /sys/devices/system/cpu/cpu0/online
+echo 1 > /sys/devices/system/cpu/cpu1/online
+echo 1 > /sys/devices/system/cpu/cpu2/online
+echo 1 > /sys/devices/system/cpu/cpu3/online
+echo 1 > /sys/devices/system/cpu/cpu4/online
+echo 1 > /sys/devices/system/cpu/cpu5/online
+echo 1 > /sys/devices/system/cpu/cpu6/online
+echo 1 > /sys/devices/system/cpu/cpu7/online
+
+cat /sys/devices/system/cpu/online
